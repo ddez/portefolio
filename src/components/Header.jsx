8b564cf19cc6bc72fa0da9a3450b2947233fcd71
Denis Desautard <denisdesautard@gmail.com>
2025-04-09 08:52:42 +0200

    Mise en place du menu hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,20 @@ const Header = () => {
   return (
     <>
       <div id="accueil"></div>
-      <Navbar bg="dark" data-bs-theme="dark" sticky="top">
+      <Navbar bg="dark" data-bs-theme="dark" sticky="top" expand="md">
         <Container>
           <Navbar.Brand href="#accueil">Accueil</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#projets">Projets</Nav.Link>
-            <Nav.Link href="#apropos">A Propos</Nav.Link>
-            <Nav.Link href="#formations">Formations</Nav.Link>
-            <Nav.Link href="#competences">Competences</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#projets">Projets</Nav.Link>
+              <Nav.Link href="#apropos">A Propos</Nav.Link>
+              <Nav.Link href="#formations">Formations</Nav.Link>
+              <Nav.Link href="#competences">Competences</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+
         </Container>
       </Navbar>
     </>
